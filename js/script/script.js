7c097b27e484b0e22d27fdb1af305d79e0f55ea7
diagana youssouf <diaganayoussouf86@gmail.com>
2021-03-16 13:35:59 +0100

    premier exercice en js

---
+++ js/script/script.js
@@ -0,0 +1,22 @@
+var monAge = 34;
+var monNomEtPrenom = "Diagana Youssouf";
+var statut = false;
+var monEcole = "Wild Code School";
+
+
+console.log (monAge);
+console.log (monNomEtPrenom );
+console.log (statut);
+console.log (monEcole);
+
+console.log(typeof(monAge));
+console.log(typeof(monNomEtPrenom));
+console.log(typeof(statut));
+console.log(typeof(monEcole));
+
+var monAge = monAge +1;
+var statut = true;
+var statut = !false;
+
+console.log (monAge);
+console.log (statut);
